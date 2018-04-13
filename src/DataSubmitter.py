d441a5165c7759916f7cc50ef461232168092337
Nicola Ferralis <feranick@hotmail.com>
2018-04-13 17:14:26 -0400

    Completely rewritten dataformat and streamlined flow.

--- src/DataSubmitter.py
+++ src/DataSubmitter.py
@@ -4,7 +4,7 @@
 **********************************************************
 *
 * DataSubmitter
-* version: 20180413a
+* version: 20180413b
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -13,6 +13,7 @@
 #print(__doc__)
 
 import sys, math, json, os.path, time
+import pandas as pd
 global MongoDBhost
 
 def main():
@@ -27,60 +28,92 @@ def main():
     #************************************
     ''' Read from T/RH sensor '''
     #************************************
-    data = DataCollector(lab)
-    collectedData = data.readSensors()
-    data.printUI()
+    dc = DataCollector(lab, lab, lab)
+    data = dc.getData()
+    dc.printUI()
+    jsonData = dc.makeJson()
     
     #************************************
-    ''' Make JSON and push to MongoDB '''
+    ''' Push to MongoDB '''
     #************************************
-    conn = DSmongoDB(data.typeData,collectedData,mongoFile)
-    print("\n JSON:\n",conn.makeJSON(),"\n")
-    print(" Pushing to MongoDB:")
+    conn = DataSubmitterMongoDB(jsonData,mongoFile)
     conn.pushToMongoDB()
 
 #************************************
-''' Class T/RH Sensor '''
+''' Class DataCollector '''
 #************************************
 class DataCollector:
-    def __init__(self, lab):
+    def __init__(self, institution, lab, equipment):
+        self.institution = institution
         self.lab = lab
+        self.equipment = equipment
         self.date = time.strftime("%Y%m%d")
         self.time = time.strftime("%H:%M:%S")
         self.ip = getIP()
-        self.collectedData = []
-        self.typeData = ["test1", "test2"]
+        self.data = []
+        self.header = ["header0", "header1"]
+        self.type = 1 #data format for MongoDB
 
     #************************************
-    ''' Read Sensors '''
+    ''' Collect Data '''
     #************************************
-    def readSensors(self):
-        self.collectedData.extend([self.lab, self.ip, self.date, self.time])
+    def getData(self):
+        self.data.extend([self.institution, self.lab, self.equipment, self.ip, self.date, self.time])
         try:
-            self.collectedData.extend([1,2])
+            self.data.extend([[1,2],[3,4]])
         except:
-            self.collectedData.extend([0.0,0.0])
-        return self.collectedData
+            self.data.extend([[0.0,0.0][0.0,0.0]])
+        return self.data
+        
+    def formatData(self, type):
+        jsonData = {}
+        for i in range(len(self.header)):
+            jsonData.update({self.header[i] : self.data[6+i]})
+        if type == 0:
+            listData = jsonData
+        else:
+            import pandas as pd
+            dfData = pd.DataFrame(jsonData)
+            dfData = dfData[[self.header[0], self.header[1]]]
+            listData = dict(dfData.to_dict(orient='split'))
+            listData['columnlabel'] = listData.pop('columns')
+            listData['output'] = listData.pop('data')
+            del listData['index']
+        return listData
+        
+    def makeJson(self):
+        jsonData = {
+            'institution' : self.data[0],
+            'lab' : self.data[1],
+            'equipment' : self.data[2],
+            'IP' : self.data[3],
+            'date' : self.data[4],
+            'time' : self.data[5],
+            }
+        jsonData.update(self.formatData(self.type))
+        print(" JSON Data:\n",jsonData)
+        return json.dumps(jsonData)
 
     #************************************
     ''' Print Values on screen '''
     #************************************
     def printUI(self):
-        print("\n Lab: ", self.lab)
+        print("\n Institution: ", self.institution)
+        print(" Lab: ", self.lab)
+        print(" Equipment: ", self.equipment)
         print(" IP: ", self.ip)
         print(" Date: ", self.date)
         print(" Time: ", self.time)
-        
-        print(" {0} = {1:0.1f} ".format(self.typeData[0], self.collectedData[4]))
-        print(" {0} = {1:0.1f} ".format(self.typeData[1], self.collectedData[5]),"\n")
+        for i in range(len(self.header)):
+            print(" {0} = {1} ".format(self.header[i], self.data[6+i]))
+        print("")
 
 #************************************
 ''' Class Database '''
 #************************************
-class DSmongoDB:
-    def __init__(self, type, data, file):
-        self.data = data
-        self.type = type
+class DataSubmitterMongoDB:
+    def __init__(self, jsonData, file):
+        self.jsonData = jsonData
         with open(file, 'r') as f:
             f.readline()
             self.hostname = f.readline().rstrip('\n')
@@ -97,7 +130,7 @@ class DSmongoDB:
         from pymongo import MongoClient
         client = MongoClient(self.hostname, int(self.port_num))
         auth_status = client[self.dbname].authenticate(self.username, self.password, mechanism='SCRAM-SHA-1')
-        print(' Authentication status = {0} \n'.format(auth_status))
+        print("\n Pushing to MongoDB: Authentication status = {0}".format(auth_status))
         return client
 
     def printAuthInfo(self):
@@ -106,24 +139,12 @@ class DSmongoDB:
         print(self.dbname)
         print(self.username)
         print(self.password)
-
-    def makeJSON(self):
-        dataj = {
-            'lab' : self.data[0],
-            'IP' : self.data[1],
-            'date' : self.data[2],
-            'time' : self.data[3],
-            '{0}'.format(self.type[0]) : '{0:0.1f}'.format(self.data[4]),
-            '{0}'.format(self.type[1]) : '{0:0.1f}'.format(self.data[5]),
-            }
-        return json.dumps(dataj)
     
     def pushToMongoDB(self):
-        jsonData = self.makeJSON()
         client = self.connectDB()
         db = client[self.dbname]
         try:
-            db_entry = db.EnvTrack.insert_one(json.loads(jsonData))
+            db_entry = db.EnvTrack.insert_one(json.loads(self.jsonData))
             print(" Data entry successful (id:",db_entry.inserted_id,")\n")
         except:
             print(" Data entry failed.\n")
